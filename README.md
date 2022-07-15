c0810be54eb3dbc822e6d44761a17a79f6366942
RikBril <riik.b@hotmail.com>
2022-07-15 23:32:07 +0200

    created an 16*16 grid

---
+++ README.md
@@ -0,0 +1,4 @@
+Project Etch a Sketch
+
+this is a project in order to learn programming. the goal is to create an online version of an etch a sketch.
+this will be done useing html, javascript en css. we will see where this project ends. 
---
+++ script.js
@@ -0,0 +1,29 @@
+const body = document.body
+body.style.backgroundColor = "lightblue"
+
+//Creates the main box in which the individual sketchboxes will be placed
+const sketchBox = document.createElement("div")
+sketchBox.className = "sketchBox"
+sketchBox.style.background = "black"
+sketchBox.style.padding = "2px"
+sketchBox.style.height = "542px";
+sketchBox.style.width = "542px";
+sketchBox.style.gap = "2px";
+sketchBox.style.display = "flex"
+sketchBox.style.justifyContent = "center"
+sketchBox.style.flexWrap = "wrap"
+body.appendChild(sketchBox)
+
+//creates the individual sketchboxes
+const boxes = 256
+for (let i = 0; i < boxes; i++) {
+    const box = document.createElement("div")
+    box.className = "box"
+    box.id = "'" + i + "'"
+    box.style.backgroundColor = "white"
+    box.style.height = "32px"
+    box.style.width = "32px"
+    sketchBox.appendChild(box)
+}
+
+
