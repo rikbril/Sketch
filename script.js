603f35bc609c4cbc7c4a87808762664bc03ce9b0
RikBril <riik.b@hotmail.com>
2022-07-19 22:07:20 +0200

    After finishing the the Odin Fundamentals course and revieuwing other peoples code i redid the entire project from scratch

--- script.js
+++ script.js
@@ -1,112 +1,73 @@
 const body = document.body
-body.style.margin = "0px"
-body.style.display = "flex"
-body.style.flexDirection = "column"
-body.style.backgroundColor = "darkgrey"
+const sketchGrid = document.getElementById("sketchGrid")
+const colorBox = document.getElementById("colorBox")
+const colorPicker = document.querySelector("#colorSelect")
+const getColor = document.getElementById("getColor")
+const ereaser = document.getElementById("ereaser")
+const fillGrid = document.getElementById("fillGrid")
+const clearGrid = document.getElementById("clearGrid")
+const pixelsInGrid = document.getElementById("pixelsInGrid")
 
-//Creates the main box in which the individual sketchboxes will be placed
-function sketchBoxFunc(container, gridSize) {
-    const sketchBox = document.createElement("div")
-    sketchBox.className = "sketchBox"
-    sketchBox.style.background = "black"
-    sketchBox.style.height = gridSize+"px";
-    sketchBox.style.width = gridSize + "px";
-    sketchBox.style.margin = "50px"
-    sketchBox.style.display = "flex"
-    sketchBox.style.justifyContent = "center"
-    sketchBox.style.flexWrap = "wrap"
-    sketchBox.style.border = "5px solid darkslategrey"
-    container.appendChild(sketchBox)
-    return sketchBox
+let inkt = "#000000"
+let tempInkt = ""
+let sampleColor = false
+let pixelCount = 24
+let pixelCountChecker = 24
+
+const click = document.addEventListener("mousedown", clickEvent)
+
+colorPicker.addEventListener("input", (e) => { inkt = e.target.value })
+getColor.addEventListener("click", () => {sampleColor = true})
+fillGrid.addEventListener("click", () => { fillGridWithColor(inkt) })
+clearGrid.addEventListener("click", () => { tempInkt = inkt; inkt = "white"; console.log(inkt); fillGridWithColor(inkt);  inkt = tempInkt})
+
+
+function clickEvent(e) {
+    if (e.target.className == "gridBox") {
+        if (!sampleColor) {
+            changeColorOfBox(e.target.id)
+        } else {
+            console.log("sample color")
+            sampleColor = false
+            inkt = document.getElementById(e.target.id).style.backgroundColor
+            colorPicker.style.background = inkt
+            console.log(colorPicker)
+        }
+    }
 }
-//creates the individual sketchboxes
-function boxes(sketchBox, pixelCount, boxes) {
-    for (let i = 0; i < (boxes * boxes); i++) {
+
+function fillGridWithColor(inkt) {
+    for (let i =0; i < (pixelCountChecker*pixelCountChecker); i++) {
+        changeColorOfBox(i)
+    }
+}
+
+function changeColorOfBox(targetId) {
+    document.getElementById(targetId).style.background = inkt
+}
+
+function deleteChilds(parent) {
+    const count = parent.childElementCount
+    for (let i =0; i < count; i++) {
+       sketchGrid.removeChild(sketchGrid.lastElementChild)
+    }   
+}
+function populateGrid(pixelCount) {
+    let pixelSize = Math.floor(900 / pixelCount)
+    deleteChilds(sketchGrid)
+    sketchGrid.style.height = (pixelCount * pixelSize) + "px"
+    sketchGrid.style.width = (pixelCount * pixelSize) + "px"
+    for (let i = 0; i < (pixelCount * pixelCount); i++) {
         const box = document.createElement("div")
-        box.className = "box"
-        box.id = "'" + i + "'"
-        box.style.height = pixelCount + "px"
-        box.style.width = pixelCount + "px"
-        sketchBox.appendChild(box)
-    }    
-}
-//creates header
-function headerFunc() {
-    const header = document.createElement("header")
-    header.style.margin = "15px"
-    header.style.alignSelf = "center"
-    header.style.fontSize = "40px"
-    header.className = "header"
-    header.textContent = "Pixelated E-sketch"
-    header.style.backgroundColor = "grey"
-    header.style.padding = "35px"
-    header.style.paddingLeft = "900px"
-    header.style.paddingRight = "900px"
-    body.appendChild(header)
-    console.log("appended header")
-}
-//creates container 
-function containerFunc() {
-    const container = document.createElement("div")
-    container.className = "container"
-    container.style.display = "flex"
-    container.style.justifySelf = "center"
-    container.style.alignSelf = "center"
-    body.appendChild(container)
-    console.log("appended container")
-    return container
-}
-//creates ui --red
-function uiFunc(container) {
-    const ui = document.createElement("div")
-    ui.className = "ui"
-    ui.style.display = "flex"
-    ui.style.flexDirection = "column"
-    ui.style.backgroundColor = "red"
-    ui.style.margin = "100px"
-    container.appendChild(ui)
-    return ui
-}
-//creates buttons --brown
-function buttons(ui) {
-    const array = ["color", "colour grabber", "erease field", "fill field","box size", "grid size"]
-    for (let i = 0; i < array.length; i++) {
-        const button = document.createElement("button")
-        button.className = "option"
-        button.style.width = "250px"
-        button.style.margin = "10px"
-        button.textContent = array[i]
-        button.style.backgroundColor = "brown"
-        ui.appendChild(button)
+        box.style.height = pixelSize + "px"
+        box.style.width = pixelSize + "px"
+        box.style.padding = "0px"
+        box.style.margin = "0px"
+        box.className = "gridBox"
+        box.id = i
+        box.style.backgroundColor = "white"
+        sketchGrid.appendChild(box)
     }
+    pixelCountChecker = pixelCount 
 }
-
-function gridAndBoxSize(boxAmount, gridSize) {
-    const pixelCount = (Math.floor((gridSize/boxAmount)))
-    return pixelCount 
-}
-
-
-//keeps listening for hoover events in order to color the sketchbox
-const hoover = document.addEventListener("mouseover", hooverEvent)
-function hooverEvent(e) {
-    if (e.target.id) {
-        const box = document.getElementById(e.target.id)
-        box.style.backgroundColor = "green"
-        console.log(e.target.id + "pixelsize :" + box.getBoundingClientRect().width)
-    }
-}
-//runs all the functions
-function run() {
-
-    const pixelCount = gridAndBoxSize(40, 1200)
-    headerFunc()
-    const container = containerFunc()
-    const ui = uiFunc(container)
-    buttons(ui)
-    const sketchBox = sketchBoxFunc(container, 1200)
-    boxes(sketchBox, pixelCount, 40)
-    console.log(sketchBox.getBoundingClientRect().height, " ", sketchBox.getBoundingClientRect().width)
-
-    console.log("ran all the functions")
-} run()
+populateGrid(pixelCount)
